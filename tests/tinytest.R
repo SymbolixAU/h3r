f3c5e84c51ca4ffdef10149533cfb1af57090a78
dave <dcooley@symbolix.com.au>
2023-04-18 07:59:25 +1000

    restructured tinytests

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,3 +1,7 @@
-library(tinytest)
+if (requireNamespace("tinytest", quietly=TRUE)) {
 
-test_all(pkgdir = "./", testdir = "./tests/tinytest")
+  ## Set a seed to make the test deterministic
+  set.seed(42)
+  Sys.setenv("R_TESTS"="")
+  tinytest::test_package("h3r")
+}
