5326c0603b5c8305e6d4aca81d49c14ea8213690
dcooley <david.cooley141@gmail.com>
2023-05-03 10:57:49 +1000

    can't get it to build

--- inst/capi/h3rc/src/h3rc.c
+++ inst/capi/h3rc/src/h3rc.c
@@ -2,26 +2,10 @@
 #define R_NO_REMAP
 #include <R.h>
 #include <Rinternals.h>
+
 #include "h3rapi.h"
 #include "h3rc.h"
 
 SEXP h3rcLatLngToCell(SEXP lat, SEXP lon, SEXP res) {
   return h3rLatLngToCell(lat, lon, res);
 }
-
-
-// SEXP h3rcDirectionForNeighbour(SEXP origin, SEXP destination) {
-//
-//   H3Index o;
-//   stringToH3(CHAR(STRING_ELT(origin, 0)), &o);
-//   H3Index d;
-//   stringToH3(CHAR(STRING_ELT(destination, 0)), &d);
-//
-//   // Direction dir = directionForNeighbor(o, d);
-//   SEXP direction = PROTECT(Rf_allocVector(INTSXP, 1));
-//   //
-//   // SET_INTEGER_ELT(direction, 0, dir);
-//
-//   UNPROTECT(1);
-//   return direction;
-// }
--- inst/capi/h3rc/src/h3rc.h
+++ inst/capi/h3rc/src/h3rc.h
@@ -4,11 +4,6 @@
 
 #include <Rinternals.h>
 
-#include "h3rapi.h"
-
 SEXP h3rcLatLngToCell(SEXP lat, SEXP lon, SEXP res);
-
-// SEXP h3rcDirectionForNeighbour(H3Index origin, H3Index destination);
-
 
 #endif
