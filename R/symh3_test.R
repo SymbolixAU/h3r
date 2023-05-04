92a2cde7a8eae343e8adff88aeb39efde371e30d
RayShao01010010 <rshao@symbolix.com.au>
2023-05-04 16:50:05 +1000

    Some prototype code for edge coords

--- R/symh3_test.R
+++ R/symh3_test.R
@@ -15,3 +15,24 @@
 directions_cell <- function(origin, distance, direction) {
   .Call(symh3_directions_cell, origin, distance, direction)
 }
+
+
+
+# cell_path <- c("8cbe63562a54bff", directions_cell(origin = c("8cbe63562a54bff"), distance = c(5L), direction = c("I", "JK"))[[1]])
+#
+# output_df <- NULL
+# for (i in 1:(length(cell_path) - 1)) {
+#   result<- directedEdgeToBoundary(cellsToDirectedEdge(cell_path[i], cell_path[i + 1]))
+#   edge <- names(result)
+#   output_df <- rbind(output_df, data.frame(
+#     cell = cell_path[i],
+#     edge = edge,
+#     lat_1 = result[[edge]]$lat[1],
+#     lat_2 = result[[edge]]$lat[2],
+#     lng_1 = result[[edge]]$lng[1],
+#     lng_2 = result[[edge]]$lng[2],
+#     stringsAsFactors = FALSE
+#   ))
+# }
+#
+# output_df
