9a3b22087580748dd236adda9f787a4927e75b0c
RayShao01010010 <rshao@symbolix.com.au>
2023-05-05 12:41:35 +1000

    Some more prototype functions

--- R/symh3_test.R
+++ R/symh3_test.R
@@ -17,22 +17,37 @@ directions_cell <- function(origin, distance, direction) {
 }
 
 
-
-# cell_path <- c("8cbe63562a54bff", directions_cell(origin = c("8cbe63562a54bff"), distance = c(5L), direction = c("I", "JK"))[[1]])
+# library(geosphere)
+#
+# cell <- "81283ffffffffff" # Res3: 832800fffffffff
+# cell_path <- c(cell, directions_cell(origin = cell, distance = 20L, direction = "I")[[1]])
 #
 # output_df <- NULL
 # for (i in 1:(length(cell_path) - 1)) {
 #   result<- directedEdgeToBoundary(cellsToDirectedEdge(cell_path[i], cell_path[i + 1]))
 #   edge <- names(result)
+#   centre <- geosphere::midPoint(c(result[[edge]]$lng[1], result[[edge]]$lat[1]), c(result[[edge]]$lng[2], result[[edge]]$lat[2]))
 #   output_df <- rbind(output_df, data.frame(
 #     cell = cell_path[i],
 #     edge = edge,
-#     lat_1 = result[[edge]]$lat[1],
-#     lat_2 = result[[edge]]$lat[2],
-#     lng_1 = result[[edge]]$lng[1],
-#     lng_2 = result[[edge]]$lng[2],
+#     point = centre,
 #     stringsAsFactors = FALSE
 #   ))
 # }
-#
 # output_df
+#
+# output_df_2 <- NULL
+# for (i in 1:(nrow(output_df) - 1)) {
+#   orig <- c(output_df[i,]$point.lon, output_df[i,]$point.lat)
+#   dest <- c(output_df[i + 1,]$point.lon, output_df[i + 1,]$point.lat)
+#   output_df_2 <- rbind(output_df_2, data.frame(
+#     origin.lon = orig[1],
+#     origin.lat = orig[2],
+#     distance = geosphere::distHaversine(orig, dest),
+#     bearing = geosphere::bearing(orig, dest),
+#     stringsAsFactors = FALSE
+#   ))
+# }
+# print(paste0("Res: ", getResolution(cell)))
+# output_df_2
+
