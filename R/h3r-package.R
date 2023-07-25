8891ed0d33304834dcda2834a4d4bf8977a09ba8
dave <dcooley@symbolix.com.au>
2023-07-25 10:00:38 +1000

    staitons data

--- R/h3r-package.R
+++ R/h3r-package.R
@@ -2,6 +2,22 @@
 #' @import h3lib
 NULL
 
+#' Stations
+#'
+#' A data set of train stations in Melbourne
+#'
+#' @format
+#' \describe{
+#'  \item{stop_id}{The ID of the station}
+#'  \item{stop_name}{The name of the station}
+#'  \item{lat}{The latitude coordinate of the station}
+#'  \item{lon}{The longitude coordinate of the station}
+#' }
+#'
+#' @details
+#' Obtained from \url{https://discover.data.vic.gov.au/dataset/timetable-and-geographic-information-gtfs}
+#' and distributed under the Creative Commons 4 License \url{https://creativecommons.org/licenses/by/4.0/}
+"stations"
 
 int <- function(x) as.integer(x)
 
