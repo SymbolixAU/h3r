b701e62fd2e73b979f43869dd80c595524665752
dcooley <=>
2023-10-09 16:12:04 +1100

    removing 'function' defs

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,13 +2,13 @@
 
 ## Read `h3libapi.h` and write all 'inline' function defs to `init.c`
 
-lines <- readLines("~/Documents/github/h3lib/inst/include/h3libapi.h")
-fns <- lines[ grepl("inline ", lines) ]
-fns <- gsub("inline ", "", fns)
-fns <- gsub("\\(.*","",fns)
-fns <- gsub(" ", " (*", fns)
-fns <- paste0(fns, ");\n")
-cat(fns)
+# lines <- readLines("~/Documents/github/h3lib/inst/include/h3libapi.h")
+# fns <- lines[ grepl("inline ", lines) ]
+# fns <- gsub("inline ", "", fns)
+# fns <- gsub("\\(.*","",fns)
+# fns <- gsub(" ", " (*", fns)
+# fns <- paste0(fns, ");\n")
+# cat(fns)
 
 
 stations <- structure(list(stop_id = c("15351", "15353", "19827", "19828",
